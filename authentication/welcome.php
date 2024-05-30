050f68cbf06ba4a55494096f0142b4869f183f56
Anish Paudel <124338545+Anish5703@users.noreply.github.com>
2024-05-30 15:13:17 +0545

    Add files via upload

---
+++ authentication/welcome.php
@@ -0,0 +1,19 @@
+<?php 
+ session_start();
+    if(!isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn'] == false){
+        header('location: index.php');
+    }
+
+    echo "<h1> Welcome " .$_SESSION['username']. "</h1>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
